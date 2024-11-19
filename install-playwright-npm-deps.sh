a2a5af8e518396120eadcddbcacd4b65e309dd9b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-18 16:53:15 -0800

    iter

---
+++ install-playwright-npm-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+rm -rf t && mkdir t && cd t
+
+
+
+npm init -y
+npm install --save-dev @playwright/test
+echo y | npm init playwright@latest -- --browser=chromium --quiet --gha --lang=Typescript --install-deps
+
+npm install --save-dev --save-exact typescript prettier
---
+++ install-playwright-pnpm-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+rm -rf t && mkdir t && cd t
+
+corepack enable
+corepack prepare pnpm@latest --activate
+pnpm --version
+
+pnpm init
+pnpm add --save-dev @playwright/test
+pnpm create playwright --browser=chromium --quiet --gha --lang=Typescript --install-deps
+
+pnpm add --save-dev --save-exact typescript prettier
