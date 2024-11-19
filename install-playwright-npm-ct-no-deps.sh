dacecf18850d532ab964c5518f9ec2aebc870f0c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-18 16:53:46 -0800

    iter

--- install-playwright-npm-ct-no-deps.sh
+++ install-playwright-npm-ct-no-deps.sh
@@ -2,8 +2,6 @@
 set -e
 rm -rf t && mkdir t && cd t
 
-
-
 npm init -y
 npm install --save-dev @playwright/test
 echo y | npm init playwright@latest -- --ct --browser=chromium --quiet --gha --lang=Typescript --install-deps
--- install-playwright-npm-no-deps.sh
+++ install-playwright-npm-no-deps.sh
@@ -2,8 +2,6 @@
 set -e
 rm -rf t && mkdir t && cd t
 
-
-
 npm init -y
 npm install --save-dev @playwright/test
 echo y | npm init playwright@latest -- --browser=chromium --quiet --gha --lang=Typescript --install-deps
